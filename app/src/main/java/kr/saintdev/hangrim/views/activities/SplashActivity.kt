9801698794f1355eb0643a50d1b4d72b06c82d24
ballbot <5252bb@daum.net>
2018-12-19 22:19:30 +0900

    12.19 2018 Make Splash Activity.

---
+++ app/src/main/java/kr/saintdev/hangrim/views/activities/SplashActivity.kt
@@ -0,0 +1,23 @@
+package kr.saintdev.hangrim.views.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import kr.saintdev.hangrim.MainActivity
+import kr.saintdev.hangrim.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val handler = Handler {
+            startActivity(
+                Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+            true
+        }
+        handler.sendEmptyMessageDelayed(0, 2000)
+    }
+}
